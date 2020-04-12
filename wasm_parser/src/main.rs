4c2009ffd9ec7016ab72822b4615302ac0b9fee1
Kevin Per <kevin.per@protonmail.com>
2020-04-12 08:04:48 +0000

    Init docopt

--- wasm_parser/src/main.rs
+++ wasm_parser/src/main.rs
@@ -1,16 +1,50 @@
 extern crate wasm_parser;
 
+use docopt::Docopt;
+use serde::Deserialize;
 use wasm_parser::{parse, read_wasm};
 
-//use std::env;
+const USAGE: &'static str = "
+WebAssembly parser for binary files.
+
+Usage:
+  ./wasm_parser <input> [--no-output]
+  ./wasm_parser (-h | --help)
+  ./wasm_parser --version
+
+Options:
+  -h --help     Show this screen.
+  --version     Show version.
+  --no-output   Don't print
+";
+
+#[derive(Debug, Deserialize)]
+struct Args {
+    flag_no_output: bool,
+    arg_input: String,
+}
 
 fn main() {
     env_logger::init();
 
+    let args: Args = Docopt::new(USAGE)
+        .and_then(|d| d.deserialize())
+        .unwrap_or_else(|e| e.exit());
+
+    let reader = read_wasm!(args.arg_input);
+
+    let module = parse(reader);
+
+    if !args.flag_no_output {
+        println!("{:#?}", module);
+    }
+
+    /*
     //let reader = read_wasm!("./test_files/if_loop.wasm");
     let reader = read_wasm!("./test_files/arithmetic_i32.wasm");
     //let reader = read_wasm!("./test_files/empty.wasm");
 
     let _module = parse(reader);
-    //println!("{:#?}", module);
+    println!("{:#?}", module);
+    */
 }
