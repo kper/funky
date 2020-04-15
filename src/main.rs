0fc3a3d58745f11c3067bd4d291c389db5623311
Kevin Per <kevin.per@protonmail.com>
2020-04-15 16:49:48 +0000

    Init docopt

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,61 @@ mod engine;
 use crate::engine::Engine;
 use crate::engine::ModuleInstance;
 use crate::engine::Value::*;
+use docopt::Docopt;
+use serde::Deserialize;
+use validation::validate;
 use wasm_parser::{parse, read_wasm};
+
+const USAGE: &str = "
+Funky - a WebAssembly Interpreter
+
+Usage:
+  ./funky <input> [--stage0 | --stage1]
+  ./funky (-h | --help)
+  ./funky --version
+
+Options:
+  -h --help     Show this screen.
+  --version     Show version.
+  --stage0      Stop at Parser
+  --stage1      Stop at Validation
+";
+
+#[derive(Debug, Deserialize)]
+struct Args {
+    flag_stage0: bool,
+    flag_stage1: bool,
+    arg_input: String,
+}
 
 fn main() {
     env_logger::init();
-    let reader = read_wasm!("./tests/function_call.wasm");
+
+    let args: Args = Docopt::new(USAGE)
+        .and_then(|d| d.deserialize())
+        .unwrap_or_else(|e| e.exit());
+
+    let reader = read_wasm!(args.arg_input);
 
     info!("Parsing wasm file");
-    let module = parse(reader);
-    let mi = ModuleInstance::new(module.unwrap());
+
+    let module = parse(reader).unwrap();
+
+    if args.flag_stage0 {
+        println!("{:#?}", module);
+
+        return;
+    }
+
+    let validation = validate(&module);
+
+    if args.flag_stage1 {
+        println!("{:#?}", validation);
+
+        return;
+    }
+
+    let mi = ModuleInstance::new(module);
     info!("Constructing engine");
     let mut e = Engine::new(mi);
     info!("Invoking function {:?}", 1);
