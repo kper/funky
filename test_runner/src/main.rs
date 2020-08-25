79a57884d556edd29d434a628d03eafb8942ccdd
Kevin Per <kevin.per@protonmail.com>
2020-08-25 09:48:39 +0200

    Revert "Make clippy happy"
    This reverts commit 7ff1fe6d56487753e74e9551cf3c86cebf92a40b.

--- test_runner/src/main.rs
+++ test_runner/src/main.rs
@@ -247,15 +247,16 @@ fn run_spec_test(path: &DirEntry, total_stats: Arc<Stats>, cmd_arguments: &Vec<S
             // Replace `current_engine` with next WASM module
             Command::Module(m) => current_engine = fs_handler.get(&m.filename),
             Command::AssertReturn(case) => {
-                if cmd_arguments.len() > 2
-                    && cmd_arguments
+                if cmd_arguments.len() > 2 {
+                    if cmd_arguments
                         .iter()
                         .filter(|x| x.contains(&case.action.field))
                         .count()
                         == 0
-                {
-                    log::info!("Skipping {} because not matched", case.action.field);
-                    continue;
+                    {
+                        log::info!("Skipping {} because not matched", case.action.field);
+                        continue;
+                    }
                 }
 
                 total_stats.total_count.fetch_add(1, Ordering::Relaxed);
