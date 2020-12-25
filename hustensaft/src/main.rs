f3e7301f5c134ac84aac8e837b46748807afdb74
Kevin Per <kevin.per@protonmail.com>
2020-12-25 08:34:20 +0100

    Setup debugger project #31

---
+++ hustensaft/src/main.rs
@@ -0,0 +1,19 @@
+use std::io;
+use tui::Terminal;
+use tui::backend::TermionBackend;
+use termion::raw::IntoRawMode;
+use tui::widgets::{Widget, Block, Borders};
+use tui::layout::{Layout, Constraint, Direction};
+
+fn main() -> Result<(), io::Error> {
+    let stdout = io::stdout().into_raw_mode()?;
+    let backend = TermionBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    terminal.draw(|f| {
+        let size = f.size();
+        let block = Block::default()
+            .title("Block")
+            .borders(Borders::ALL);
+        f.render_widget(block, size);
+    })
+}
