7888d65571bb0e25715800315441a572462297a2
Kevin Per <kevin.per@protonmail.com>
2021-01-06 22:21:39 +0100

    Add function name to path for perf graphs

--- wolkentreiber.py
+++ wolkentreiber.py
@@ -7,8 +7,8 @@ import sys
 
 commit = subprocess.check_output(["git", "rev-parse", "--short", "HEAD"]).strip()
 
-if len(sys.argv) == 1:
-    sys.exit("You need to pass on a executable path")
+if len(sys.argv) == 2:
+    sys.exit("You need to pass on a executable path and function")
 
 with open('wolkentreiber.csv', 'r') as csv_file:
     reader = csv.reader(csv_file, delimiter=';')
@@ -18,7 +18,7 @@ with open('wolkentreiber.csv', 'r') as csv_file:
 
     payload = dict()
     payload['commit'] = commit
-    payload['path'] = sys.argv[1]
+    payload['path'] = sys.argv[1] + '#' + sys.argv[2]
 
     for row in reader:
         value=row[0]
--- wolkentreiber.sh
+++ wolkentreiber.sh
@@ -1,19 +1,20 @@
 path="./testsuite/block.0.wasm"
+func="break-bare"
 num=3
 n=0
 
 cargo build --release --bin funky
 
-
 while [ "$n" -lt $num ]; do
 	n=$(( n + 1 ))
-	perf stat -e cache-misses,branch-misses,cpu-cycles,instructions,branch-instructions -x \; -o wolkentreiber.csv  target/release/funky $path "break-bare" && ./wolkentreiber.py $path
+	perf stat -e cache-misses,branch-misses,cpu-cycles,instructions,branch-instructions -x \; -o wolkentreiber.csv  target/release/funky $path $func && ./wolkentreiber.py $path $func
 done
 
 path="./tests/gcd.wasm"
+func="gcd"
 n=0
 
 while [ "$n" -lt $num ]; do
 	n=$(( n + 1 ))
-	perf stat -e cache-misses,branch-misses,cpu-cycles,instructions,branch-instructions -x \; -o wolkentreiber.csv target/release/funky $path "gcd" "I32(640)" "I32(125483)"  && ./wolkentreiber.py $path
+	perf stat -e cache-misses,branch-misses,cpu-cycles,instructions,branch-instructions -x \; -o wolkentreiber.csv target/release/funky  $path $func "I32(640)" "I32(125483)" && ./wolkentreiber.py $path $func
 done
