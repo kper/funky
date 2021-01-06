0e3cd8b27f148bfdb71f45f3c59796cda7d1b059
Kevin Per <kevin.per@protonmail.com>
2021-01-06 17:33:26 +0100

    Add graphs in wolkentreiber

--- wolkentreiber/main.py
+++ wolkentreiber/main.py
@@ -1,6 +1,6 @@
 #!flask/bin/python
 
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, jsonify
 import sqlite3 as sql
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
@@ -14,7 +14,6 @@ class perf_run(db.Model):
     __tablename__ = 'perf_run'
 
 
-    #{'commit': b'166cfcdac03efa6bd96523e2b252a9aec477302e', 'cache-misses': '604318', 'branch-misses': '1420790', 'cpu-cycles': '336041733', 'instructions': '332257074', 'branch-instructions': '63355474'}
     id = db.Column('id', db.Integer, primary_key = True)
     commit = db.Column('commit', db.String(64))
     cache_misses = db.Column('cache-misses', db.BigInteger)
@@ -27,6 +26,7 @@ class perf_run(db.Model):
     def __init__(self, commit, cache_misses, branch_misses, cpu_cycles, instructions, branch_instructions, on_create):
         self.commit = commit
         self.cache_misses = cache_misses
+        self.branch_misses = branch_misses
         self.cpu_cycles = cpu_cycles
         self.instructions = instructions
         self.branch_instructions = branch_instructions
@@ -48,3 +48,51 @@ def test_run():
         rows = perf_run.query.all()  
         print("rows", len(rows))
         return render_template("./perf.html", rows=rows)
+
+@app.route('/commits')
+def labels():
+        rows = perf_run.query.with_entities(perf_run.commit).order_by(perf_run.on_create.asc()).all()
+
+        flatten = [item for sublist in rows for item in sublist]
+
+        return jsonify(flatten)
+
+@app.route('/cache_misses')
+def cache_misses():
+        rows = perf_run.query.with_entities(perf_run.cache_misses).order_by(perf_run.on_create.asc()).all()
+
+        flatten = [item for sublist in rows for item in sublist]
+
+        return jsonify(flatten)
+
+@app.route('/branch_misses')
+def branch_misses():
+        rows = perf_run.query.with_entities(perf_run.branch_misses).order_by(perf_run.on_create.asc()).all()
+
+        flatten = [item for sublist in rows for item in sublist]
+
+        return jsonify(flatten)
+
+@app.route('/cpu_cycles')
+def cpu_cycles():
+        rows = perf_run.query.with_entities(perf_run.cpu_cycles).order_by(perf_run.on_create.asc()).all()
+
+        flatten = [item for sublist in rows for item in sublist]
+
+        return jsonify(flatten)
+
+@app.route('/instructions')
+def instructions():
+        rows = perf_run.query.with_entities(perf_run.instructions).order_by(perf_run.on_create.asc()).all()
+
+        flatten = [item for sublist in rows for item in sublist]
+
+        return jsonify(flatten)
+
+@app.route('/branch_instructions')
+def branch_instructions():
+        rows = perf_run.query.with_entities(perf_run.branch_instructions).order_by(perf_run.on_create.asc()).all()
+
+        flatten = [item for sublist in rows for item in sublist]
+
+        return jsonify(flatten)
