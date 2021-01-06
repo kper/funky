dafd7b32aed119dbf6d3dfe9a20c1816bf052b83
Kevin Per <kevin.per@protonmail.com>
2021-01-06 22:14:06 +0100

    Run wolkentreiber without cargo and use the release build

--- wolkentreiber.sh
+++ wolkentreiber.sh
@@ -1,10 +1,13 @@
 path="./testsuite/block.0.wasm"
 num=3
-
 n=0
+
+cargo build --release --bin funky
+
+
 while [ "$n" -lt $num ]; do
 	n=$(( n + 1 ))
-	perf stat -e cache-misses,branch-misses,cpu-cycles,instructions,branch-instructions -x \; -o wolkentreiber.csv  cargo run --bin funky -- $path "break-bare" && ./wolkentreiber.py $path
+	perf stat -e cache-misses,branch-misses,cpu-cycles,instructions,branch-instructions -x \; -o wolkentreiber.csv  target/release/funky $path "break-bare" && ./wolkentreiber.py $path
 done
 
 path="./tests/gcd.wasm"
@@ -12,5 +15,5 @@ n=0
 
 while [ "$n" -lt $num ]; do
 	n=$(( n + 1 ))
-	perf stat -e cache-misses,branch-misses,cpu-cycles,instructions,branch-instructions -x \; -o wolkentreiber.csv  cargo run --bin funky -- $path "gcd" "I32(640)" "I32(125483)"  && ./wolkentreiber.py $path
+	perf stat -e cache-misses,branch-misses,cpu-cycles,instructions,branch-instructions -x \; -o wolkentreiber.csv target/release/funky $path "gcd" "I32(640)" "I32(125483)"  && ./wolkentreiber.py $path
 done
