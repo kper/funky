b8d249579a4691b825a991f4783e81bcd6c4a35f
Kevin Per <kevin.per@protonmail.com>
2021-02-22 15:27:42 +0100

    Implements opcodes

--- ide/src/ssa/mod.rs
+++ ide/src/ssa/mod.rs
@@ -444,6 +444,73 @@ impl IR {
                 OP_F64_CONST(a) => {
                     writeln!(self.buffer, "%{} = {}", self.counter.get(), a).unwrap();
                 }
+                OP_I32_CLZ
+                | OP_I32_CTZ
+                | OP_I32_POPCNT
+                | OP_I64_CLZ
+                | OP_I64_CTZ
+                | OP_I64_POPCNT
+                | OP_F32_ABS
+                | OP_F32_NEG
+                | OP_F32_CEIL
+                | OP_F32_FLOOR
+                | OP_F32_TRUNC
+                | OP_F32_NEAREST
+                | OP_F32_SQRT
+                | OP_F64_ABS
+                | OP_F64_NEG
+                | OP_F64_CEIL
+                | OP_F64_FLOOR
+                | OP_F64_TRUNC
+                | OP_F64_NEAREST
+                | OP_F64_SQRT
+                | OP_I32_WRAP_I64
+                | OP_I32_TRUNC_F32_S
+                | OP_I32_TRUNC_F32_U
+                | OP_I32_TRUNC_F64_S
+                | OP_I32_TRUNC_F64_U
+                | OP_I64_EXTEND_I32_U
+                | OP_I64_EXTEND_I32_S
+                | OP_I64_TRUNC_F32_S
+                | OP_I64_TRUNC_F32_U
+                | OP_I64_TRUNC_F64_S
+                | OP_I64_TRUNC_F64_U
+                | OP_F32_CONVERT_I32_S
+                | OP_F32_CONVERT_I32_U
+                | OP_F32_CONVERT_I64_S
+                | OP_F32_CONVERT_I64_U
+                | OP_F32_DEMOTE_F64
+                | OP_F64_CONVERT_I32_S
+                | OP_F64_CONVERT_I32_U
+                | OP_F64_CONVERT_I64_S
+                | OP_F64_CONVERT_I64_U
+                | OP_F64_PROMOTE_F32
+                | OP_I32_REINTERPRET_F32
+                | OP_I64_REINTERPRET_F64
+                | OP_F32_REINTERPRET_I32
+                | OP_F64_REINTERPRET_I64
+                | OP_I32_EXTEND8_S
+                | OP_I32_EXTEND16_S
+                | OP_I64_EXTEND8_S
+                | OP_I64_EXTEND16_S
+                | OP_I64_EXTEND32_S
+                | OP_I32_TRUNC_SAT_F32_S
+                | OP_I32_TRUNC_SAT_F32_U
+                | OP_I32_TRUNC_SAT_F64_S
+                | OP_I32_TRUNC_SAT_F64_U
+                | OP_I64_TRUNC_SAT_F32_S
+                | OP_I64_TRUNC_SAT_F32_U
+                | OP_I64_TRUNC_SAT_F64_S
+                | OP_I64_TRUNC_SAT_F64_U => {
+                    writeln!(
+                        self.buffer,
+                        "%{} = {} %{}",
+                        self.counter.get(),
+                        "op",
+                        self.counter.peek() - 2,
+                    )
+                    .unwrap();
+                }
 
                 OP_I32_ADD | OP_I32_SUB | OP_I32_MUL | OP_I32_DIV_S | OP_I32_DIV_U
                 | OP_I32_REM_S | OP_I32_REM_U | OP_I32_AND | OP_I32_OR | OP_I32_XOR
@@ -456,7 +523,10 @@ impl IR {
                 | OP_I64_EQZ | OP_I64_EQ | OP_I64_NE | OP_I64_LT_S | OP_I64_LT_U | OP_I64_GT_S
                 | OP_I64_GT_U | OP_I64_LE_S | OP_I64_LE_U | OP_I64_GE_S | OP_I64_GE_U
                 | OP_F32_EQ | OP_F32_NE | OP_F32_LT | OP_F32_GT | OP_F32_LE | OP_F32_GE
-                | OP_F64_EQ | OP_F64_NE | OP_F64_LT | OP_F64_GT | OP_F64_LE | OP_F64_GE => {
+                | OP_F64_EQ | OP_F64_NE | OP_F64_LT | OP_F64_GT | OP_F64_LE | OP_F64_GE
+                | OP_F32_ADD | OP_F32_SUB | OP_F32_MUL | OP_F32_DIV | OP_F64_ADD | OP_F64_SUB
+                | OP_F64_MUL | OP_F64_DIV | OP_F32_MIN | OP_F32_MAX | OP_F32_COPYSIGN
+                | OP_F64_MIN | OP_F64_MAX | OP_F64_COPYSIGN => {
                     writeln!(
                         self.buffer,
                         "%{} = %{} {} %{}",
