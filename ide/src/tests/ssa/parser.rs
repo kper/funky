d3ecafec7c7e3a9d482d9f855f9c5a8ad7123187
Kevin Per <kevin.per@protonmail.com>
2021-03-10 11:41:20 +0100

    Add `KILL` to grammar

--- ide/src/tests/ssa/parser.rs
+++ ide/src/tests/ssa/parser.rs
@@ -32,6 +32,7 @@ fn parse_instruction() {
     assert!(InstructionParser::new().parse("BR TABLE GOTO 7 5 ELSE GOTO 3").is_ok());
     assert!(InstructionParser::new().parse("%a = op %b").is_ok());
     assert!(InstructionParser::new().parse("%a = %b op %c").is_ok());
+    assert!(InstructionParser::new().parse("KILL %0").is_ok());
 }
 
 #[test]
