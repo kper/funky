7a40db5d0ebcb93fd0fa4ef5ceb97c6ad06d015e
Kevin Per <kevin.per@protonmail.com>
2021-03-23 07:49:35 +0100

    Disable clippy for taint solver

--- ci.sh
+++ ci.sh
@@ -1,1 +1,1 @@
-cargo build --verbose --all && cargo test --verbose --all && cargo clippy --all-features -- -D warnings && cd ide && cargo build && cargo test --verbose && cargo clippy --all-features -- -D warnings
+cargo build --verbose --all && cargo test --verbose --all && cargo clippy --all-features -- -D warnings && cd ide && cargo build && cargo test --verbose
