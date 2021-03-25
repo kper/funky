1c8713dd5524b2917dd7ae12800b1806f072f579
Kevin Per <kevin.per@protonmail.com>
2021-03-25 17:17:04 +0100

    Add benchmarks

--- ide/benches/bench.rs
+++ ide/benches/bench.rs
@@ -38,25 +38,33 @@ fn bench_fib<T: GraphReachability>(solver: &mut IfdsSolver<T>, graph: &mut Graph
     solver.all_sinks(graph, req);
 }
 
-fn criterion_benchmark(c: &mut Criterion) {
-    let engine = wasm!("../tests/fib.wasm");
-    let mut ir = IR::new();
-    ir.visit(&engine).unwrap();
+macro_rules! benchmark {
+    ($name:ident) => {
+        fn $name(c: &mut Criterion) {
+            let engine = wasm!(format!("../tests/{}.wasm", stringify!($name)));
+            let mut ir = IR::new();
+            ir.visit(&engine).unwrap();
 
-    let mut convert = Convert::new();
-    let prog = ProgramParser::new().parse(&ir.buffer()).unwrap();
-    let mut graph = convert.visit(&prog).unwrap();
-    let mut solver = IfdsSolver::new(BFS);
+            let mut convert = Convert::new();
+            let prog = ProgramParser::new().parse(&ir.buffer()).unwrap();
+            let mut graph = convert.visit(&prog).unwrap();
+            let mut solver = IfdsSolver::new(BFS);
 
-    let req = Request {
-        variable: "%0".to_string(),
-        function: "0".to_string(),
-        pc: 1,
+            let req = Request {
+                variable: "%0".to_string(),
+                function: "0".to_string(),
+                pc: 1,
+            };
+            c.bench_function(stringify!($name), |b| {
+                b.iter(|| bench_fib(&mut solver, &mut graph, &req))
+            });
+        }
     };
-    c.bench_function("fib", |b| {
-        b.iter(|| bench_fib(&mut solver, &mut graph, &req))
-    });
 }
 
-criterion_group!(benches, criterion_benchmark);
+benchmark!(fib);
+benchmark!(fac);
+benchmark!(blocks);
+
+criterion_group!(benches, fib, fac, blocks);
 criterion_main!(benches);
