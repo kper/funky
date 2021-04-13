16a06956d9873dbc204ceb33fcec3ad29e061949
Kevin Per <kevin.per@protonmail.com>
2021-04-13 21:11:20 +0200

    Adding unit test for multiple parameters

--- ifds/src/tests/unit/handle_call.rs
+++ ifds/src/tests/unit/handle_call.rs
@@ -34,7 +34,7 @@ fn setup() -> (
 
 #[test]
 fn test_normal_argument_passing() {
-    let (mut convert, mut graph, mut path_edge, mut worklist, mut end_summary, mut normal_flows) =
+    let (mut convert, mut graph, mut path_edge, mut worklist, _end_summary, mut normal_flows) =
         setup();
 
     let mut state = State::default();
@@ -117,8 +117,137 @@ fn test_normal_argument_passing() {
 }
 
 #[test]
+fn test_normal_argument_passing_with_multiple_params() {
+    let (mut convert, mut graph, mut path_edge, mut worklist,  _end_summary, mut normal_flows) =
+        setup();
+
+    let mut state = State::default();
+
+    let caller_function = AstFunction {
+        name: "main".to_string(),
+        results_len: 0,
+        definitions: vec!["%0".to_string(), "%1".to_string(), "%2".to_string()],
+        instructions: vec![
+            Instruction::Const("%0".to_string(), 0.0),
+            Instruction::Call(
+                "test".to_string(),
+                vec!["%0".to_string(), "%1".to_string()],
+                vec!["%2".to_string()],
+            ),
+        ],
+        ..Default::default()
+    };
+
+    let callee_function = AstFunction {
+        name: "test".to_string(),
+        params: vec!["%0".to_string(), "%1".to_string()],
+        results_len: 1,
+        definitions: vec!["%0".to_string(), "%1".to_string()],
+        instructions: vec![Instruction::Return(vec!["%0".to_string()])],
+    };
+
+    let caller_init_facts = state.init_function(&caller_function, 0).unwrap();
+
+    let _ = convert
+        .pacemaker(
+            &caller_function,
+            &mut graph,
+            &mut path_edge,
+            &mut worklist,
+            &mut normal_flows,
+            &caller_init_facts,
+            &mut state,
+        )
+        .unwrap();
+
+    let foo = Fact {
+        belongs_to_var: "%0".to_string(),
+        function: "main".to_string(),
+        next_pc: 1, //this is an edge case
+        track: 1,
+        ..Default::default()
+    };
+
+    let _ = state.cache_fact(&caller_function.name, foo.clone());
+
+    let bar = Fact {
+        belongs_to_var: "%1".to_string(),
+        function: "main".to_string(),
+        next_pc: 1, //this is an edge case
+        track: 2,
+        ..Default::default()
+    };
+
+    let _ = state.cache_fact(&caller_function.name, bar.clone());
+
+    // first param
+    {
+        let call_to_start_edges = convert
+            .pass_args(
+                &caller_function,
+                &callee_function,
+                &vec!["%0".to_string(), "%1".to_string()],
+                &mut graph,
+                1,
+                &"%0".to_string(),
+                &mut normal_flows,
+                &mut path_edge,
+                &mut worklist,
+                &mut state,
+            )
+            .unwrap();
+
+        assert_eq!(call_to_start_edges.len(), 1);
+        assert_eq!(
+            call_to_start_edges,
+            vec![Edge::Call {
+                from: foo,
+                to: Fact {
+                    belongs_to_var: "%0".to_string(),
+                    function: "test".to_string(),
+                    track: 1,
+                    ..Default::default()
+                }
+            }]
+        );
+    }
+
+    // second param
+    {
+        let call_to_start_edges = convert
+            .pass_args(
+                &caller_function,
+                &callee_function,
+                &vec!["%0".to_string(), "%1".to_string()],
+                &mut graph,
+                1,
+                &"%1".to_string(),
+                &mut normal_flows,
+                &mut path_edge,
+                &mut worklist,
+                &mut state,
+            )
+            .unwrap();
+
+        assert_eq!(call_to_start_edges.len(), 1);
+        assert_eq!(
+            call_to_start_edges,
+            vec![Edge::Call {
+                from: bar,
+                to: Fact {
+                    belongs_to_var: "%1".to_string(),
+                    function: "test".to_string(),
+                    track: 2,
+                    ..Default::default()
+                }
+            }]
+        );
+    }
+}
+
+#[test]
 fn test_normal_argument_passing_taut() {
-    let (mut convert, mut graph, mut path_edge, mut worklist, mut end_summary, mut normal_flows) =
+    let (mut convert, mut graph, mut path_edge, mut worklist, _end_summary, mut normal_flows) =
         setup();
 
     let mut state = State::default();
