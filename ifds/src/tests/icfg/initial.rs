0a609965240227e8ac402323285370794ece129f
Kevin Per <kevin.per@protonmail.com>
2021-04-15 14:06:47 +0200

    Implement LOAD instruction for initial flow function

--- ifds/src/tests/icfg/initial.rs
+++ ifds/src/tests/icfg/initial.rs
@@ -535,9 +535,82 @@ fn test_call_initial_flow() {
     let (edges, normal_flows) = flow(
         0,
         vec!["%0", "%1"],
-        vec![
-            Instruction::Call("test".to_string(), vec!["%0".to_string()], vec!["%1".to_string()]),
-        ],
+        vec![Instruction::Call(
+            "test".to_string(),
+            vec!["%0".to_string()],
+            vec!["%1".to_string()],
+        )],
+    );
+
+    assert_eq!(edges.len(), 2);
+
+    assert_eq!(
+        Some(&Edge::Path {
+            from: Fact {
+                var_is_taut: true,
+                next_pc: 0,
+                belongs_to_var: "taut".to_string(),
+                function: "main".to_string(),
+                ..Default::default()
+            },
+            to: Fact {
+                var_is_taut: true,
+                next_pc: 0,
+                belongs_to_var: "taut".to_string(),
+                function: "main".to_string(),
+                ..Default::default()
+            },
+        }),
+        edges.get(0),
+    );
+    assert_eq!(
+        Some(&Edge::Path {
+            from: Fact {
+                var_is_taut: true,
+                next_pc: 0,
+                belongs_to_var: "taut".to_string(),
+                function: "main".to_string(),
+                ..Default::default()
+            },
+            to: Fact {
+                next_pc: 1,
+                belongs_to_var: "%1".to_string(),
+                function: "main".to_string(),
+                track: 2,
+                ..Default::default()
+            },
+        }),
+        edges.get(1)
+    );
+
+    assert_eq!(
+        Some(&Edge::Normal {
+            from: Fact {
+                var_is_taut: true,
+                next_pc: 0,
+                belongs_to_var: "taut".to_string(),
+                function: "main".to_string(),
+                ..Default::default()
+            },
+            to: Fact {
+                next_pc: 1,
+                belongs_to_var: "%1".to_string(),
+                function: "main".to_string(),
+                track: 2,
+                ..Default::default()
+            },
+            curved: false
+        }),
+        normal_flows.get(0)
+    );
+}
+
+#[test]
+fn test_memory_load_initial_flow() {
+    let (edges, normal_flows) = flow(
+        0,
+        vec!["%0", "%1"],
+        vec![Instruction::Load("%1".to_string(), 0.0, "%0".to_string())],
     );
 
     assert_eq!(edges.len(), 2);
