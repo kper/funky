d48074d5388f460c15ebf5f8bda24219df355268
Kevin Per <kevin.per@protonmail.com>
2021-05-01 12:01:38 +0200

    Refactor code for the initial flows (sparse)

--- ifds/src/icfg/flowfuncs/sparse_taint/initial.rs
+++ ifds/src/icfg/flowfuncs/sparse_taint/initial.rs
@@ -19,10 +19,10 @@ impl SparseInitialFlowFunction for SparseTaintInitialFlowFunction {
         // We need the offset, because not every
         // instruction is taintable. For example BLOCK and JUMP
         // have no registers. That's why skip to the next one.
-        let offset = 0;
+        let mut offset = 0;
         let instructions = &function.instructions;
 
-        let init_fact = init_facts.get(0).context("Cannot find taut")?.clone();
+        let mut init_fact = init_facts.get(0).context("Cannot find taut")?.clone();
 
         debug!("init fact is {:#?}", init_fact);
 
@@ -40,180 +40,56 @@ impl SparseInitialFlowFunction for SparseTaintInitialFlowFunction {
                 | Instruction::Phi(dest, _, _)
                 | Instruction::Conditional(dest, _)
                 | Instruction::Load(dest, _, _) => {
-                    let before = vec![init_fact.clone()];
-
-                    for b in before.into_iter() {
-                        let after_taut = defuse
-                            .demand(ctx, &function, &"taut".to_string(), b.pc)
-                            .context("Cannot find taut's fact")?;
-
-                        debug!("Next taut is {:#?}", after_taut);
-
-                        for taut in after_taut.into_iter() {
-                            edges.push(Edge::Path {
-                                from: init_fact.clone().clone(),
-                                to: taut.clone(),
-                            });
-                        }
-
-                        let after_var = defuse
-                            .demand_inclusive(ctx, &function, dest, pc)
-                            .context("Cannot find var's fact")?
-                            .into_iter()
-                            .map(|x| x.clone())
-                            .collect::<Vec<_>>();
-
-                        // append all left sides to the nodes
-                        // %2 = binop %0 %1 -- there %2 is the left side
-                        let mut appended = Vec::new();
-                        let mut append_lhs = |dest: &String| -> Result<()> {
-                            defuse.force_remove_if_outdated(function, dest, pc)?;
-                            let x = defuse.demand_inclusive(ctx, function, dest, pc)?;
-                            appended.extend(x.into_iter().map(|x| x.clone()));
-
-                            Ok(())
-                        };
-
-                        for var in after_var.clone().iter() {
-                            append_lhs(&var.belongs_to_var)?;
-                        }
-
-                        for var in after_var.into_iter().chain(appended) {
-                            let applied = var.apply_bound();
-
-                            // after
-                            edges.push(Edge::Path {
-                                from: init_fact.clone().clone(),
-                                to: applied.clone(),
-                            });
-                        }
-                    }
+                    self.generate_normal_flows(
+                        ctx,
+                        function,
+                        defuse,
+                        &mut edges,
+                        &mut init_fact,
+                        pc,
+                        &vec![dest.to_string()],
+                    )?;
                 }
                 Instruction::Call(_callee, _, dests) => {
-                    // When the analysis starts at a Call Instruction,
-                    // then the function doesn't get called.
-                    // But all destinations get automatically tainted.
-
-                    let before = vec![init_fact.clone()];
-                    for b in before.into_iter() {
-                        let after_taut = defuse
-                            .demand(ctx, &function, &"taut".to_string(), b.pc)
-                            .context("Cannot find taut's fact")?;
-
-                        debug!("Next taut is {:#?}", after_taut);
-
-                        for taut in after_taut.into_iter() {
-                            edges.push(Edge::Path {
-                                from: init_fact.clone().clone(),
-                                to: taut.clone(),
-                            });
-                        }
-                    }
-                    for dest in dests {
-                        let after_var = defuse
-                            .demand(ctx, &function, dest, pc)
-                            .context("Cannot find var's fact")?
-                            .into_iter()
-                            .map(|x| x.clone())
-                            .collect::<Vec<_>>();
-
-                        // append all left sides to the nodes
-                        // %2 = binop %0 %1 -- there %2 is the left side
-                        let mut appended = Vec::new();
-                        let mut append_lhs = |dest: &String| -> Result<()> {
-                            defuse.force_remove_if_outdated(function, dest, pc)?;
-                            let x = defuse.demand_inclusive(ctx, function, dest, pc)?;
-                            appended.extend(x.into_iter().map(|x| x.clone()));
-
-                            Ok(())
-                        };
-
-                        for var in after_var.clone().iter() {
-                            append_lhs(&var.belongs_to_var)?;
-                        }
-
-                        for var in after_var.into_iter().chain(appended) {
-                            let applied = var.apply_bound();
-
-                            // after
-                            edges.push(Edge::Path {
-                                from: init_fact.clone().clone(),
-                                to: applied.clone(),
-                            });
-                        }
-                    }
+                    self.generate_normal_flows(
+                        ctx,
+                        function,
+                        defuse,
+                        &mut edges,
+                        &mut init_fact,
+                        pc,
+                        &dests,
+                    )?;
                 }
                 Instruction::Store(_src, offset, _) => {
-                    let before = vec![init_fact.clone()];
-
-                    for b in before.into_iter() {
-                        // The tautological fact was built by the `pacemaker`
-                        // and will not be sparsely propagated.
-
-                        let after_taut = defuse
-                            .demand(ctx, &function, &"taut".to_string(), pc)
-                            .context("Cannot find taut's fact")?;
-
-                        for taut in after_taut.into_iter() {
-                            edges.push(Edge::Path {
-                                from: init_fact.clone().clone(),
-                                to: taut.clone(),
-                            });
-                        }
-
+                    let mem = {
                         let mem = ctx
                             .state
                             .add_memory_var(function.name.clone(), offset.clone() as usize);
 
-                        let after_var = defuse
-                            .demand_inclusive(ctx, &function, &mem.name, pc)
-                            .context("Cannot find var's fact")?;
+                        mem.name.clone()
+                    };
 
-                        for var in after_var.into_iter() {
-                            let mut applied = var.apply_bound();
-
-                            assert!(applied.pc <= applied.next_pc);
-
-                            edges.push(Edge::Path {
-                                from: init_fact.clone().clone(),
-                                to: applied.clone(),
-                            });
-                        }
-                    }
+                    self.generate_normal_flows(
+                        ctx,
+                        function,
+                        defuse,
+                        &mut edges,
+                        &mut init_fact,
+                        pc,
+                        &vec![mem],
+                    )?;
                 }
                 Instruction::Return(_dests) => {
-                    let before = vec![init_fact.clone()];
-
-                    for b in before.into_iter() {
-                        // The tautological fact was built by the `pacemaker`
-                        // and will not be sparsely propagated.
-
-                        let after_taut = defuse
-                            .demand(ctx, &function, &"taut".to_string(), pc)
-                            .context("Cannot find taut's fact")?;
-
-                        for taut in after_taut.into_iter() {
-                            edges.push(Edge::Path {
-                                from: init_fact.clone().clone(),
-                                to: taut.clone(),
-                            });
-                        }
-
-                        let after_var = defuse
-                            .demand(ctx, &function, &"taut".to_string(), pc)
-                            .context("Cannot find var's fact")?;
-
-                        for var in after_var.into_iter() {
-                            let applied = var.apply_bound();
-
-                            assert!(applied.pc <= applied.next_pc);
-
-                            edges.push(Edge::Path {
-                                from: init_fact.clone().clone(),
-                                to: var.clone(),
-                            });
-                        }
-                    }
+                    self.generate_normal_flows(
+                        ctx,
+                        function,
+                        defuse,
+                        &mut edges,
+                        &mut init_fact,
+                        pc,
+                        &vec![],
+                    )?;
                 }
                 Instruction::Block(_) | Instruction::Jump(_) => {
                     panic!("Block or Jump as first instruction is not supported");
@@ -225,5 +101,71 @@ impl SparseInitialFlowFunction for SparseTaintInitialFlowFunction {
         }
 
         Ok(edges)
+    }
+}
+
+impl SparseTaintInitialFlowFunction {
+    /// Helper function for generating normal flows for the initial statement.
+    /// ## Parameters
+    ///  - `init_fact` is the first fact of the procedure.
+    ///  - `dests` is a list of Registers which are on lhs side.
+    pub(crate) fn generate_normal_flows(
+        &self,
+        ctx: &mut Ctx,
+        function: &AstFunction,
+        defuse: &mut DefUseChain,
+        edges: &mut Vec<Edge>,
+        init_fact: &mut Fact,
+        pc: usize,
+        dests: &Vec<String>,
+    ) -> Result<()> {
+        let after_taut = defuse
+            .demand(ctx, &function, &"taut".to_string(), init_fact.pc)
+            .context("Cannot find taut's fact")?;
+
+        debug!("Next taut is {:#?}", after_taut);
+
+        for taut in after_taut.into_iter() {
+            edges.push(Edge::Path {
+                from: init_fact.clone().clone(),
+                to: taut,
+            });
+        }
+
+        for dest in dests {
+            let after_var = defuse
+                .demand_inclusive(ctx, &function, dest, pc)
+                .context("Cannot find var's fact")?
+                .into_iter()
+                .map(|x| x.clone())
+                .collect::<Vec<_>>();
+
+            // append all left sides to the nodes
+            // %2 = binop %0 %1 -- there %2 is the left side
+            let mut appended = Vec::new();
+            let mut append_lhs = |dest: &String| -> Result<()> {
+                defuse.force_remove_if_outdated(function, dest, pc)?;
+                let x = defuse.demand_inclusive(ctx, function, dest, pc)?;
+                appended.extend(x.into_iter().map(|x| x.clone()));
+
+                Ok(())
+            };
+
+            for var in after_var.clone().iter() {
+                append_lhs(&var.belongs_to_var)?;
+            }
+
+            for var in after_var.into_iter().chain(appended) {
+                let applied = var.apply_bound();
+
+                // after
+                edges.push(Edge::Path {
+                    from: init_fact.clone().clone(),
+                    to: applied.clone(),
+                });
+            }
+        }
+
+        Ok(())
     }
 }
